7b0c93b3b4e4a66f35a624581eb37264277ab9e5
Simon Otter <simonotter@gmail.com>
2020-11-24 14:46:36 +0000

    Added s3 signed urls to createImage

--- src/lambda/http/createImage.ts
+++ src/lambda/http/createImage.ts
@@ -6,8 +6,14 @@ import * as uuid from 'uuid'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+});
+
 const groupsTable = process.env.GROUPS_TABLE
 const imagesTable = process.env.IMAGES_TABLE
+const bucketName = process.env.IMAGES_S3_BUCKET;
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION;
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
@@ -30,13 +36,16 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   const imageId = uuid.v4();
   const newItem = await createImage(groupId, imageId, event);
 
+  const url = getUploadUrl(imageId);
+
   return {
     statusCode: 201,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      newItem: newItem
+      newItem: newItem,
+      uploadUrl: url
     })
   }
 }
@@ -64,7 +73,8 @@ async function createImage(groupId: string, imageId: string, event: any) {
     groupId: groupId,
     timestamp: timestamp,
     imageId: imageId,
-    ...newImage
+    ...newImage,
+    imageUrl: 'https://${bucketName}.s3.amazonaws.com/${imageId}'
   }
 
   console.log('Storing new item:', newItem);
@@ -75,4 +85,12 @@ async function createImage(groupId: string, imageId: string, event: any) {
   }).promise();
 
   return newItem;
+}
+
+function getUploadUrl(imageId: string) {
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: imageId,
+    Expires: urlExpiration
+  })
 }
