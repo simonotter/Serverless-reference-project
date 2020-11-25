5cc1b58036545b7e0262b13b99adae350c93b686
Simon Otter <simonotter@gmail.com>
2020-11-25 16:14:11 +0000

    Added DynamoDB Stream handler

---
+++ src/lambda/dynamoDb/elasticSearchSync.ts
@@ -0,0 +1,11 @@
+import { DynamoDBStreamEvent, DynamoDBStreamHandler } from 'aws-lambda';
+import 'source-map-support/register';
+
+export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent) => {
+  console.log('Processing eents batch from DynamoDB: ', JSON.stringify(event));
+
+  for (const record of event.Records) {
+    console.log('Processing record: ', JSON.stringify(record));
+    
+  }
+}
