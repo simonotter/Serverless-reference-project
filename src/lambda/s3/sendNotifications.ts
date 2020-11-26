a308f9e50bd9734ea0fbc9ae9b01b5b77a4d17fb
Simon Otter <simonotter@gmail.com>
2020-11-26 11:24:54 +0000

    Converted S3 lambda to use SNS events

--- src/lambda/s3/sendNotifications.ts
+++ src/lambda/s3/sendNotifications.ts
@@ -1,4 +1,4 @@
-import { S3Handler, S3Event } from 'aws-lambda';
+import { S3Event, SNSEvent, SNSHandler } from 'aws-lambda';
 import 'source-map-support/register';
 import * as AWS from 'aws-sdk';
 
@@ -16,8 +16,21 @@ const connectionParams = {
 
 const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams);
 
-export const handler: S3Handler = async (event: S3Event) => {
-  for (const record of event.Records) {
+export const handler: SNSHandler = async (event: SNSEvent) => {
+  console.log('Processing SNS event ', JSON.stringify(event));
+
+  for (const snsRecord of event.Records) {
+    const s3EventStr = snsRecord.Sns.Message;
+    console.log('Processing S3 event ', s3EventStr);
+    const s3Event = JSON.parse(s3EventStr);
+
+    await processS3Event(s3Event);    
+  }
+  
+}
+
+async function processS3Event (s3Event: S3Event) {
+  for (const record of s3Event.Records) {
     const key = record.s3.object.key;
     console.log('Processing S3 item with key: ', key);
 
