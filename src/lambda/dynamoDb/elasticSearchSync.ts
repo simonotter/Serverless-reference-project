8183eab691068e6df107a8ba6cf6577a3c0d4b2d
Simon Otter <simonotter@gmail.com>
2020-11-26 09:59:15 +0000

    Implemented elasticsearch index writing

--- src/lambda/dynamoDb/elasticSearchSync.ts
+++ src/lambda/dynamoDb/elasticSearchSync.ts
@@ -1,5 +1,14 @@
 import { DynamoDBStreamEvent, DynamoDBStreamHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import * as elasticsearch from 'elasticsearch';
+import * as httpAwsEs from 'http-aws-es';
+
+const esHost = process.env.ES_ENDPOINT;
+
+const es = new elasticsearch.Client({
+  host: [ esHost ],
+  connectionClass: httpAwsEs
+})
 
 export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent) => {
   console.log('Processing eents batch from DynamoDB: ', JSON.stringify(event));
@@ -7,5 +16,28 @@ export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent)
   for (const record of event.Records) {
     console.log('Processing record: ', JSON.stringify(record));
     
+    if (record.eventName !== 'INSERT') {
+      continue;
+    }
+
+    const newItem = record.dynamodb.NewImage;
+
+    const imageId = newItem.imageId.S;
+
+    const body = {
+      imageId: newItem.imageId.S,
+      groupId: newItem.groupId.S,
+      imageUrl: newItem.imageUrl.S,
+      title: newItem.title.S,
+      timestamp: newItem.timestamp.S
+    }
+
+    await es.index({
+      index: 'images-index',
+      type: 'images',
+      id: imageId,
+      body
+    });
+
   }
 }
